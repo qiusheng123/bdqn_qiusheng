d60c247a8725644fad3b407ba98cf04f1cbacacd
qiusheng <304493841@qq.com>
2019-07-12 10:37:48 +0800

    修改

--- java_Test/src/Day02/test02.java
+++ java_Test/src/Day02/test02.java
@@ -3,16 +3,17 @@ package Day02;
  * 2、有一个计算器，能够计算两个数字的和，三个数字的和，四个数字的和。五个数字的和
  */
 public class test02 {
-	public void add(int num1,int num2) {
-		System.out.println(num1+num2);
+	public int add(int num1,int num2) {
+		return num1+num2;
 	}
-	public void add(int num1,int num2,int num3) {
-		System.out.println(num1+num2+num3);
+	public int add(int num1,int num2,int num3) {
+		return num1+num2+num3;
 	}
-	public void add(int num1,int num2,int num3,int num4) {
-		System.out.println(num1+num2+num3+num4);
+	public int add(int num1,int num2,int num3,int num4) {
+		return num1+num2+num3+num4;
 	}
-	public void add(int num1,int num2,int num3,int num4,int num5) {
-		System.out.println(num1+num2+num3+num4+num5);
+	public int add(int num1,int num2,int num3,int num4,int num5) {
+		return num1+num2+num3+num4+num5;
 	}
+	
 }
---
+++ java_Test/src/Day02/test05.java
@@ -0,0 +1,22 @@
+package Day02;
+/*
+ * 5、在控制台输出如下：（依据二维数组提供循环次数）：
+1 2 3 4 5 6
+6 5 4 3 2 1
+6 5 4 3 2 1
+1 2 3 4 5 6
+
+ */
+public class test05 {
+
+	public static void main(String[] args) {
+		int arr[][]=new int[4][6];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				
+			}
+		}
+		
+	}
+
+}
