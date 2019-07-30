19071f6c4e7b52079ddf4172aa8ab97e03630c90
qiusheng <304493841@qq.com>
2019-07-30 11:08:13 +0800

    修改继承

--- java_Test/src/person/Student.java
+++ java_Test/src/person/Student.java
@@ -1,24 +1,10 @@
 package person;
 
-public class Student {
+public class Student extends Person {
 	String stuNo;//学号
-	String name;//姓名
-	int age;//年级
-	int height;//身高
-	private String tel;//电话
 	
-	public String getName() {
-		return name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public int getHeight() {
-		return height;
-	}
-
 	public String getStuNo() {
 		return stuNo;	
 	}
-
+	
 }
--- java_Test/src/person/Teacher.java
+++ java_Test/src/person/Teacher.java
@@ -1,24 +1,11 @@
 package person;
 
-public class Teacher{
+public class Teacher extends Person{
 	String subject;//学科
-	String name;//姓名
-	int age;//年级
-	int height;//身高
-	private String tel;//电话
 	
-	public String getName() {
-		return name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public int getHeight() {
-		return height;
-	}
-
 	public String getSubject() {
 		return subject;
 	}
-
+	
+	
 }
